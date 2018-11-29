18f65dcead5de142f0f38f1b26f423ff557c6034
skybber <lada.dvorak7@gmail.com>
2018-11-29 20:52:41 +0100

    Add --disable-test-image to windows build
    to skip test image building since it is too slow.

--- build_windows.sh
+++ build_windows.sh
@@ -15,4 +15,4 @@ unset -v CXX
 
 cd ./openjdk-build
 export LOG=info
-./makejdk-any-platform.sh --tag "${SOURCE_JDK_TAG}" --build-variant dcevm --branch dcevm11 --configure-args '-disable-warnings-as-errors --disable-hotspot-gtest' --target-file-name java11-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk11u
+./makejdk-any-platform.sh --tag "${SOURCE_JDK_TAG}" --build-variant dcevm --branch dcevm11 --disable-test-image --configure-args '-disable-warnings-as-errors --disable-hotspot-gtest' --target-file-name java11-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk11u
