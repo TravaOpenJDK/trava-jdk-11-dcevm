b0bff23dbba60ab814fc82dc4fc3f4122f15a3bf
skybber <lada.dvorak7@gmail.com>
2018-12-01 19:34:15 +0100

    Use Adopt v2 API for download

--- build_windows.sh
+++ build_windows.sh
@@ -1,13 +1,8 @@
 export PATH=/cygdrive/c/tools/cygwin/bin:$PATH
 
-url=$(curl -silent https://api.adoptopenjdk.net/openjdk10/nightly/x64_Win/ | grep 'binary_link' | grep -Eo '(http|https)://[^"]+' | head -1)
-wget --quiet ${url}
-zip_file=$(basename ${url})
-target=$(unzip -Z1 ${zip_file} | grep 'bin/javac'  | tr '/' '\n' | tail -3 | head -1)
-unzip -q ${zip_file}
-
-export JAVA_HOME=$(cd "${target}"; pwd)
-export PATH=${JAVA_HOME}/bin:$PATH 
+wget -q -O OpenJDK10_x64_Windows.zip "https://api.adoptopenjdk.net/v2/binary/releases/openjdk10?os=windows&release=latest&arch=x64"
+JDK_BOOT_DIR=$PWD/$(unzip -Z1 OpenJDK10_x64_Windows.zip | grep 'bin/javac'  | tr '/' '\n' | tail -3 | head -1)
+unzip -q OpenJDK10_x64_Windows.zip
 
 # unset cygwin's gcc preconfigured
 unset -v CC
@@ -15,4 +10,4 @@ unset -v CXX
 
 cd ./openjdk-build
 export LOG=info
-./makejdk-any-platform.sh --tag "${SOURCE_JDK_TAG}" --build-variant dcevm --branch dcevm11 --hswap-agent-download-url ${HSWAP_AGENT_DOWNLOAD_URL} --disable-test-image --configure-args '-disable-warnings-as-errors --disable-hotspot-gtest' --target-file-name java11-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk11u
+./makejdk-any-platform.sh --tag "${SOURCE_JDK_TAG}" --build-variant dcevm --branch dcevm11 --jdk-boot-dir ${JDK_BOOT_DIR} --hswap-agent-download-url ${HSWAP_AGENT_DOWNLOAD_URL} --disable-test-image --configure-args '-disable-warnings-as-errors --disable-hotspot-gtest' --target-file-name java11-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk11u
