7536a9ccd8588c3384fc90356bc66752f56eeeed
Vladimir Dvorak <lada.dvorak7@gmail.com>
2019-11-05 22:52:56 +0100

    Fix modified openjdk11 structure

--- build_windows.sh
+++ build_windows.sh
@@ -1,8 +1,8 @@
 export PATH=/cygdrive/c/tools/cygwin/bin:$PATH
 
-wget -q -O OpenJDK10_x64_Windows.zip "https://api.adoptopenjdk.net/v2/binary/releases/openjdk11?os=windows&release=jdk-11.0.3%2B7&arch=x64&type=jdk"
-JDK_BOOT_DIR=$PWD/$(unzip -Z1 OpenJDK10_x64_Windows.zip | grep 'bin/javac'  | tr '/' '\n' | tail -3 | head -1)
-unzip -q OpenJDK10_x64_Windows.zip
+wget -q -O OpenJDK11_x64_Windows.zip "https://api.adoptopenjdk.net/v2/binary/releases/openjdk11?os=windows&release=jdk-11.0.3%2B7&arch=x64&type=jdk"
+JDK_BOOT_DIR=$PWD/$(unzip -Z1 OpenJDK11_x64_Windows.zip | grep 'bin/javac'  | tr '/' '\n' | tail -3 | head -1)
+unzip -q OpenJDK11_x64_Windows.zip
 
 # unset cygwin's gcc preconfigured
 unset -v CC
