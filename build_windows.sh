7f5bacda005a3abed00cf44502802c728c9d8616
Vladimir Dvorak <lada.dvorak7@gmail.com>
2021-04-27 20:15:04 +0200

    dcevm-11.0.11+1

--- build_windows.sh
+++ build_windows.sh
@@ -11,4 +11,4 @@ export JAVA_HOME=${JDK_BOOT_DIR}
 
 cd ./openjdk-build
 export LOG=info
-./makejdk-any-platform.sh --tag "${SOURCE_JDK_TAG}" --build-variant dcevm  --branch dcevm11-jdk-11.0.10-adopt --jdk-boot-dir ${JDK_BOOT_DIR} --hswap-agent-download-url ${HSWAP_AGENT_DOWNLOAD_URL} --hswap-agent-core-download-url ${HSWAP_AGENT_CORE_DOWNLOAD_URL} --disable-test-image --check-fingerprint false --configure-args '-disable-warnings-as-errors --disable-hotspot-gtest' --target-file-name java11-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk11u
+./makejdk-any-platform.sh --tag "${SOURCE_JDK_TAG}" --build-variant dcevm  --branch dcevm11-jdk-11.0.11-adopt --jdk-boot-dir ${JDK_BOOT_DIR} --hswap-agent-download-url ${HSWAP_AGENT_DOWNLOAD_URL} --hswap-agent-core-download-url ${HSWAP_AGENT_CORE_DOWNLOAD_URL} --disable-test-image --check-fingerprint false --configure-args '-disable-warnings-as-errors --disable-hotspot-gtest' --target-file-name java11-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk11u
